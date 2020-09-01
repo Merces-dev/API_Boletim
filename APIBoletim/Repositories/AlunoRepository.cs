f4b3614b8062b71617d9ac203a21450cddabc2fb
Giovani Merces <giovanitjs013@gmail.com>
2020-09-01 17:20:16 -0300

    first commit

---
+++ APIBoletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,41 @@
+﻿using APIBoletim.Domains;
+using APIBoletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Aluno BuscarPorId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
